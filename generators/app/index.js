659496bbb7f9b034d347c621b2711f3ff2f52b32
Ian Bull <irbull@eclipsesource.com>
2016-01-20 19:32:26 -0800

    Create a basic package.json for TabrisJS apps
    This will generate a basic package.json with name, description
    and entryPoint. The tabris dependency will also be installed.

---
+++ generators/app/index.js
@@ -0,0 +1,42 @@
+var generators = require('yeoman-generator');
+var input = {};
+
+module.exports = generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+    this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }, {
+      type    : 'input',
+      name    : 'entrypoint',
+      message : 'Project entry point',
+      default : 'app.js' // Default to current folder name
+    }, {
+      type    : 'input',
+      name    : 'description',
+      message : 'Project description'
+    }], function (answers) {
+      this.log("Creating Tabris.js app: " + answers.name +
+               ", description: " + answers.description +
+               " entry point: "  + answers.entrypoint);
+      input = answers;
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      { name: input.name, description: input.description, entrypoint: input.entrypoint }
+    );
+  },
+
+  installingTabrisJS: function() {
+    this.npmInstall(['tabris'], { 'save': true });
+  }
+  
+});
