16e39588435ba57c93a335c83098ba694664a4fe
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-03-09 18:27:48 +0100

    Add basic tests for the generator
    The existing generator tests don't actually test the generator
    but only the presence of some source files.
    
    Add basic test for the creation of files and the contents of
    package.json and config.xml.

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,60 @@
+import {join} from 'path';
+import {readFileSync} from 'fs';
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
+import helpers from 'yeoman-test';
 import assert from 'yeoman-assert';
-import * as generator from '../app/index';
 
-describe('Package files:', () => {
+describe('Generator (JS)', function() {
 
-  it('exists on the file system', () => {
-    assert.file(['package.json', '.eslintrc', '.babelrc', '.editorconfig', 'README.md', 'app/index.js']);
+  beforeEach(function() {
+    return helpers.run(join(__dirname, '../app')).withPrompts({
+      proj_type: 'js',
+      name: 'foo'
+    });
   });
 
-  it('contains the tabris homepage in the package.json', () => {
-    assert.fileContent('package.json', /"homepage": "http:\/\/tabrisjs.com\/"/);
+  it('creates package.json with correct content', function() {
+    let json = JSON.parse(readFileSync('package.json', {encoding: 'utf-8'}));
+    expect(json.name).to.equal('foo');
+    expect(json.version).to.equal('0.1.0');
+    expect(json.main).to.equal('src/app.js');
+    expect(json.scripts.start).to.equal('tabris serve');
   });
 
-  it('contains an entry point in the package.json', () => {
-    assert.fileContent('package.json', /"main": "app\/index.js"/);
+  it('creates config.xml with correct id and version', function() {
+    assert.fileContent('cordova/config.xml', '<widget id="example.foo" version="0.1.0">');
   });
 
-  it('contains licence info in the README', () => {
-    assert.fileContent('README.md', /License/);
+  it('creates other files', function() {
+    assert.file(['.gitignore', 'src/app.js']);
   });
 
-  it('exports an object from the generator (app/index.js)', () => {
-    expect(generator).to.be.a('Object');
+});
+
+describe('Generator (TS)', function() {
+
+  beforeEach(function() {
+    return helpers.run(join(__dirname, '../app')).withPrompts({
+      proj_type: 'ts',
+      name: 'foo'
+    });
+  });
+
+  it('creates config.xml with correct id and version', function() {
+    assert.fileContent('cordova/config.xml', '<widget id="example.foo" version="0.1.0">');
+  });
+
+  it('creates package.json with correct content', function() {
+    let json = JSON.parse(readFileSync('package.json', {encoding: 'utf-8'}));
+    expect(json.name).to.equal('foo');
+    expect(json.version).to.equal('0.1.0');
+    expect(json.main).to.equal('dist/app.js');
+    expect(json.scripts.start).to.equal('tabris serve');
+  });
+
+  it('creates other files', function() {
+    assert.file(['.gitignore', 'src/app.ts']);
   });
 
 });
--- test/utilities.test.js
+++ test/utilities.test.js
@@ -2,72 +2,72 @@ import {describe, it} from 'mocha';
 import {expect} from 'chai';
 import {toId, toAppId, toName, isValidAppId} from '../app/utilities';
 
-describe('Generator utilities:', () => {
+describe('Generator utilities:', function() {
 
-  describe('toId', () => {
+  describe('toId', function() {
 
-    it('translates a single word to lowercase', () => {
+    it('translates a single word to lowercase', function() {
       expect(toId('MyApp')).to.equal('myapp');
     });
 
-    it('replaces all spaces with dashes, translates to lowercase', () => {
+    it('replaces all spaces with dashes, translates to lowercase', function() {
       expect(toId('My Little App')).to.equal('my-little-app');
       expect(toId('My  Little\tApp')).to.equal('my-little-app');
     });
 
-    it('reduces subsequent dashes to a single one', () => {
+    it('reduces subsequent dashes to a single one', function() {
       expect(toId('My Little - App')).to.equal('my-little-app');
     });
 
   });
 
-  describe('toAppId', () => {
+  describe('toAppId', function() {
 
-    it('translates a single word to lowercase', () => {
+    it('translates a single word to lowercase', function() {
       expect(toAppId('MyApp')).to.equal('myapp');
     });
 
-    it('replaces spaces with periods, translates to lowercase', () => {
+    it('replaces spaces with periods, translates to lowercase', function() {
       expect(toAppId('My Little App')).to.equal('my.little.app');
       expect(toAppId('My  Little\tApp')).to.equal('my.little.app');
     });
 
   });
 
-  describe('toName', () => {
+  describe('toName', function() {
 
-    it('replaces periods, dashes, and underscores with spaces, translates to titlecase', () => {
+    it('replaces periods, dashes, and underscores with spaces, translates to titlecase', function() {
       expect(toName('my.little.app')).to.equal('My Little App');
       expect(toName('my-little-app')).to.equal('My Little App');
     });
 
-    it('reduces subsequent spaces to single space', () => {
+    it('reduces subsequent spaces to single space', function() {
       expect(toName('My  Little\tApp')).to.equal('My Little App');
     });
 
   });
 
-  describe('isValidAppId', () => {
+  describe('isValidAppId', function() {
 
-    it('accepts valid app id, words separated by periods', () => {
+    it('accepts valid app id, words separated by periods', function() {
       expect(isValidAppId('my.app.id')).to.be.true;
     });
 
-    it('accepts id with two segments', () => {
+    it('accepts id with two segments', function() {
       expect(isValidAppId('my.app')).to.be.true;
     });
 
-    it('rejects id without periods (single word)', () => {
+    it('rejects id without periods (single word)', function() {
       expect(isValidAppId('myapp')).to.be.false;
     });
 
-    it('rejects ids with illegal periods', () => {
+    it('rejects ids with illegal periods', function() {
       expect(isValidAppId('my..app.id')).to.be.false;
       expect(isValidAppId('.my.app.id')).to.be.false;
       expect(isValidAppId('my.app.id.')).to.be.false;
     });
 
-    it('rejects ids with invalid characters', () => {
+    it('rejects ids with invalid characters', function() {
       expect(isValidAppId('my-app-id')).to.be.false;
       expect(isValidAppId('my app id')).to.be.false;
       expect(isValidAppId('my*app*id')).to.be.false;
