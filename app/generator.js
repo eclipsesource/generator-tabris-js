18deea178748e7afbba26e2c67b5d97c5726f8e3
Ralf Sternberg <rsternberg@eclipsesource.com>
2017-03-13 11:48:36 +0100

    Fail with error message on old Node.js versions
    When trying to run ES6 code on old Node.js versions, the resulting
    error message usually points at some lexical error in a source file.
    
    Provide a clear error message to point users with outdated Node.js
    versions in the right direction.
    
    Replace index.js with a ES5-compatible delegator module that performs
    the version check.
    
    A test would be hard to write as the test suite itself is based on ES6.

---
+++ app/generator.js
@@ -0,0 +1,127 @@
+const Generator = require('yeoman-generator');
+const {toId, toAppId, toName, isValidAppId} = require('./utilities.js');
+
+const PROJECT_TYPES = [{
+  name: 'JavaScript App',
+  value: 'js'
+}, {
+  name: 'TypeScript App',
+  value: 'ts'
+}];
+
+module.exports = class extends Generator {
+
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname, // defaults to current working dir
+        filter: toId
+      }, {
+        type: 'input',
+        name: 'version',
+        message: 'Initial version',
+        default: '0.1.0'
+      }, {
+        type: 'list',
+        name: 'proj_type',
+        message: 'Type of project',
+        choices: PROJECT_TYPES
+      }, {
+        type: 'confirm',
+        name: 'prep_build',
+        message: 'Prepare app build',
+        default: true
+      }, {
+        when: answers => answers.prep_build,
+        type: 'input',
+        name: 'app_id',
+        message: 'App ID',
+        default: answers => 'example.' + toAppId(answers.name),
+        validate: input => isValidAppId(input) ||
+          'Invalid App ID, use alphanumeric characters and periods only, EG: com.domain.app'
+      }, {
+        when: answers => answers.prep_build,
+        type: 'input',
+        name: 'app_name',
+        message: 'App name',
+        default: answers => toName(answers.name)
+      }, {
+        when: answers => answers.prep_build,
+        type: 'input',
+        name: 'app_description',
+        message: 'App description',
+        default: 'Example Tabris.js App'
+      }, {
+        when: answers => answers.prep_build,
+        type: 'input',
+        name: 'author_name',
+        message: 'Author',
+        default: this.user.git.name
+      }, {
+        when: answers => answers.prep_build,
+        type: 'input',
+        name: 'author_email',
+        message: 'Email',
+        default: this.user.git.email
+      }
+    ]).then(answers => {
+      let main = answers.proj_type === 'js' ? 'src/app.js' : 'dist/app.js';
+      this._props = Object.assign(answers, {main});
+    });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      this._props
+    );
+    if (this._props.prep_build) {
+      this.fs.copyTpl(
+        this.templatePath('cordova'),
+        this.destinationPath('cordova'),
+        this._props
+      );
+    }
+    if (this._props.proj_type === 'ts') {
+      this.fs.copyTpl(
+        this.templatePath('ts/_gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copyTpl(
+        this.templatePath('ts/src'),
+        this.destinationPath('src'),
+        this._props
+      );
+      this.fs.copyTpl(
+        this.templatePath('ts/tsconfig.json'),
+        this.destinationPath('tsconfig.json')
+      );
+    } else if (this._props.proj_type === 'js') {
+      this.fs.copyTpl(
+        this.templatePath('js/_gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copyTpl(
+        this.templatePath('js/src'),
+        this.destinationPath('src'),
+        this._props
+      );
+    }
+  }
+
+  install() {
+    this.npmInstall(['tabris@2.0.0-beta2'], {
+      save: true
+    });
+    if (this._props.proj_type === 'ts') {
+      this.npmInstall(['typescript'], {
+        saveDev: true
+      });
+    }
+  }
+
+};
