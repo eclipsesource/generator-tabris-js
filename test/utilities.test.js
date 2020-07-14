eclipsesource/generator-tabris-js

commit 6548ae314a8c96191650df6a5b864c677e2272c2

Fix JSX example not installing tsc
Change-Id: I762621bde50f40b2cb7340a68ae55be5c6ea7443

app/generator.js
@@ -64,7 +64,7 @@ module.exports = class extends Generator {
       choices: CONFIG
     }
     ]).then(answers => {
-      const tsc = !answers.template.startsWith('js');
+      const tsc = answers.template !== 'js';
       const tabrisLatest = this._npmVersion('tabris@latest').pop();
       this._props = {
         app_id: answers.app_id,

test/generator.test.js
@@ -123,7 +123,99 @@ describe('Generator', function() {
 
   });
 
-  describe('generates TypeScript-compiled projects', function() {
+  describe('generates tsc-compiled JavaScript/JSX projects', function() {
+
+    before(() => runGenerator({type: 'ts', example: 'jsx'}));
+
+    it('creates config.xml with correct id and version', function() {
+      assert.fileContent('cordova/config.xml', /<widget id=".+.foo" version="0.1.0">/);
+    });
+
+    it('creates package.json with correct content', function() {
+      const json = JSON.parse(readFileSync('package.json', {encoding: 'utf-8'}));
+      expect(json.main).to.equal('dist');
+      expect(json.scripts.lint).to.equal('eslint --ext .js,.jsx,.ts,.tsx src');
+      expect(json.scripts.test).to.equal('npm run build && npm run lint');
+      expect(json.scripts.mocha).to.be.undefined;
+      expect(json.scripts.build).to.equal('tsc -p .');
+      expect(json.scripts.start).to.equal('tabris serve -a -w');
+      expect(json.scripts.watch).to.equal('tsc -p . -w --preserveWatchOutput --inlineSourceMap');
+    });
+
+    it('creates extensions.json with correct content', function() {
+      const json = JSON.parse(readFileSync('.vscode/extensions.json', {encoding: 'utf-8'}));
+      expect(json.recommendations).to.deep.equal(['dbaeumer.vscode-eslint']);
+    });
+
+    it('creates launch.json with correct content', function() {
+      const json = JSON.parse(readFileSync('.vscode/launch.json', {encoding: 'utf-8'}));
+      expect(json.configurations.length).to.equal(1);
+    });
+
+    it('creates settings.json with correct content', function() {
+      const json = JSON.parse(readFileSync('.vscode/settings.json', {encoding: 'utf-8'}));
+      expect(json).to.deep.equal({
+        'typescript.tsdk': 'node_modules/typescript/lib',
+        'javascript.implicitProjectConfig.experimentalDecorators': true,
+        'eslint.enable': true
+      });
+    });
+
+    it('creates .tabrisignore with correct content', function() {
+      const files = readFileSync('.tabrisignore', {encoding: 'utf-8'}).split('\n');
+      expect(files).to.include('src/');
+      expect(files).to.not.include('test/');
+      expect(files).to.include('.eslintrc');
+      expect(files).to.include('tsconfig.json');
+      expect(files).to.include('.gitignore');
+      expect(files).to.include('.vscode/');
+      expect(files).to.include('node_modules/**/*.d.ts');
+      expect(files).to.include('node_modules/tabris/ClientMock.js');
+      expect(files).to.include('README.md');
+      expect(files).not.to.include('package-lock.json');
+    });
+
+    it('creates .gitignore with correct content', function() {
+      const files = readFileSync('.gitignore', {encoding: 'utf-8'}).split('\n');
+      expect(files).to.include('/dist/');
+      expect(files).to.include('/build/');
+      expect(files).to.include('node_modules/');
+    });
+
+    it('creates README.md with correct content', function() {
+      const readme = readFileSync('README.md', {encoding: 'utf-8'});
+      expect(readme).to.include('# foo');
+      expect(readme).to.include('## Run');
+      expect(readme).to.include('## Test');
+      expect(readme).to.include('## Debugging');
+      expect(readme).to.include('## Build');
+      expect(readme).not.to.include('unit tests');
+      expect(readme).to.match(/https:\/\/docs\.tabris\.com\/3\.[0-9]\/developer-app\.html/);
+      expect(readme).to.match(/https:\/\/docs\.tabris\.com\/3\.[0-9]\/debug\.html#android/);
+      expect(readme).to.match(/https:\/\/docs\.tabris\.com\/3\.[0-9]\/debug\.html#ios/);
+    });
+
+    it('creates other files', function() {
+      assert.file([
+        '.eslintrc',
+        'src/index.jsx',
+        '.vscode/tasks.json'
+      ]);
+      assert.noFile([
+        'test/tsconfig.json',
+        'test/sandbox.ts',
+        'test/sandbox.test.ts',
+        'test/App.test.ts'
+      ]);
+    });
+
+    it('lints jsx example', async function() {
+      expect(lint('src')).to.include({warningCount: 0, errorCount: 0});
+    });
+
+  });
+
+  describe('generates TypeScript projects', function() {
 
     describe('without mocha tests', function() {
 
@@ -211,12 +303,6 @@ describe('Generator', function() {
           'test/App.test.ts'
         ]);
       });
-
-      it('lints js example', async function() {
-        await runGenerator({type: 'ts', example: 'js', keepNodeModules: true});
-        expect(lint('src')).to.include({warningCount: 0, errorCount: 0});
-      });
-
       it('lints jsx example', async function() {
         await runGenerator({type: 'ts', example: 'jsx', keepNodeModules: true});
         expect(lint('src')).to.include({warningCount: 0, errorCount: 0});

commit 92333c6e2772332e8cf0ddd3078cb5810ff969a9

Fix default template copying wrong files
Introduced by 6548ae314a8c96191650df6a5b864c677e2272c2.

Create dedicated utility functions for determining the properties of
each template and write unit tests for them. This is useful since the
main generator integration tests are too slow to cover every possible
combination.

Move the template descriptions object in to a separate module for better
testing.

Change-Id: If5d6906531ef1127cec6e633f854e84efbeffeca

app/generator.js
@@ -1,25 +1,16 @@
 const path = require('path');
 const Generator = require('yeoman-generator');
-const {toAppId, toName, isValidAppId} = require('./utilities.js');
+const {
+  toAppId,
+  toName,
+  isValidAppId,
+  templateRequiresDecorators,
+  templateRequiresTsc,
+  templateSupportsMocha
+} = require('./utilities.js');
+const {TEMPLATES} = require('./templates.data');
 
 const VERSION_REGEXP = /'([2-3]\..*)'/;
-
-const TEMPLATES = [{
-  name: 'Model-View-ViewModel (TypeScript/JSX)',
-  value: 'mvvm'
-}, {
-  name: 'Model-View-Presenter (TypeScript/JSX)',
-  value: 'mvp'
-}, {
-  name: 'Hello World (TypeScript/JSX)',
-  value: 'tsx'
-}, {
-  name: 'Hello World (JavaScript/JSX)',
-  value: 'jsx'
-}, {
-  name: 'Hello World (JavaScript)',
-  value: 'js'
-}];
 
 const CONFIG = [
   {
@@ -32,7 +23,7 @@ const CONFIG = [
     short: 'Mocha',
     value: 'mocha',
     disabled: ({template}) =>
-      (!template || template.startsWith('js')) ? 'Disabled: TypeScript only' : false
+      templateSupportsMocha(template) ? false : 'Disabled: TypeScript only'
   }
 ];
 
@@ -64,7 +55,7 @@ module.exports = class extends Generator {
       choices: CONFIG
     }
     ]).then(answers => {
-      const tsc = answers.template !== 'js';
+      const tsc = templateRequiresTsc(answers.template);
       const tabrisLatest = this._npmVersion('tabris@latest').pop();
       this._props = {
         app_id: answers.app_id,
@@ -164,7 +155,7 @@ module.exports = class extends Generator {
     this.npmInstall(['tabris@' + version], {
       savePrefix: '~'
     });
-    if (this._props.example === 'mvp' || this._props.example === 'mvvm') {
+    if (templateRequiresDecorators(this._props.example)) {
       this.npmInstall(['tabris-decorators@' + version], {
         savePrefix: '~'
       });

app/templates.data.js
@@ -1,0 +1,16 @@
+exports.TEMPLATES = [{
+  name: 'Model-View-ViewModel (TypeScript/JSX)',
+  value: 'mvvm'
+}, {
+  name: 'Model-View-Presenter (TypeScript/JSX)',
+  value: 'mvp'
+}, {
+  name: 'Hello World (TypeScript/JSX)',
+  value: 'tsx'
+}, {
+  name: 'Hello World (JavaScript/JSX)',
+  value: 'jsx'
+}, {
+  name: 'Hello World (JavaScript)',
+  value: 'js'
+}];

app/utilities.js
@@ -1,7 +1,14 @@
 /*
  * Utility functions for convenience
  */
-module.exports = {toAppId, toName, isValidAppId};
+module.exports = {
+  toAppId,
+  toName,
+  isValidAppId,
+  templateRequiresTsc,
+  templateSupportsMocha,
+  templateRequiresDecorators
+};
 
 const VALID_APP_ID_REGEX = /^([a-z0-9_]+\.)+[a-z][a-z0-9_]+$/i;
 
@@ -25,4 +32,25 @@ function toName(string) {
 
 function isValidAppId(string) {
   return VALID_APP_ID_REGEX.test(string);
+}
+
+/**
+ * @param {string} templateId
+ */
+function templateRequiresTsc(templateId) {
+  return !!templateId && (templateId !== 'js');
+}
+
+/**
+ * @param {string} templateId
+ */
+function templateSupportsMocha(templateId) {
+  return templateRequiresTsc(templateId) && templateId !== 'jsx';
+}
+
+/**
+ * @param {string} templateId
+ */
+function templateRequiresDecorators(templateId) {
+  return templateSupportsMocha(templateId) && templateId !== 'tsx';
 }

test/utilities.test.js
@@ -1,6 +1,16 @@
+const {statSync, existsSync} = require('fs');
+const {resolve} = require('path');
 const {describe, it} = require('mocha');
 const {expect} = require('chai');
-const {toAppId, toName, isValidAppId} = require('../app/utilities');
+const {
+  toAppId,
+  toName,
+  isValidAppId,
+  templateRequiresTsc,
+  templateSupportsMocha,
+  templateRequiresDecorators
+} = require('../app/utilities');
+const {TEMPLATES} = require('../app/templates.data');
 
 describe('Generator utilities:', function() {
 
@@ -83,6 +93,61 @@ describe('Generator utilities:', function() {
       expect(isValidAppId('my.app.!d')).to.be.false;
       expect(isValidAppId('my.@pp.id')).to.be.false;
       expect(isValidAppId('m$.app.id')).to.be.false;
+    });
+
+  });
+
+  describe('templateRequiresTsc', function() {
+
+    it('returns correct values', function() {
+      expect(templateRequiresTsc(null)).to.be.false;
+      expect(templateRequiresTsc('mvvm')).to.be.true;
+      expect(templateRequiresTsc('mvp')).to.be.true;
+      expect(templateRequiresTsc('tsx')).to.be.true;
+      expect(templateRequiresTsc('jsx')).to.be.true;
+      expect(templateRequiresTsc('js')).to.be.false;
+    });
+
+    it('matches template description', function() {
+      TEMPLATES.forEach(template => {
+        const expected = template.name.endsWith('(TypeScript/JSX)')
+          || template.name.endsWith('(JavaScript/JSX)');
+        expect(templateRequiresTsc(template.value)).to.equal(expected);
+      });
+    });
+
+  });
+
+  describe('templateSupportsMocha', function() {
+
+    it('returns correct values', function() {
+      expect(templateSupportsMocha(null)).to.be.false;
+      expect(templateSupportsMocha('mvvm')).to.be.true;
+      expect(templateSupportsMocha('mvp')).to.be.true;
+      expect(templateSupportsMocha('tsx')).to.be.true;
+      expect(templateSupportsMocha('jsx')).to.be.false;
+      expect(templateSupportsMocha('js')).to.be.false;
+    });
+
+    it('matches template folders', function() {
+      TEMPLATES.forEach(template => {
+        const path = resolve(__dirname, '../app/templates-3.x/ts/test-' + template.value);
+        const stat = existsSync(path) && statSync(path);
+        expect(templateSupportsMocha(template.value)).to.equal(stat && stat.isDirectory());
+      });
+    });
+
+  });
+
+  describe('templateRequiresDecorators', function() {
+
+    it('returns correct values', function() {
+      expect(templateRequiresDecorators(null)).to.be.false;
+      expect(templateRequiresDecorators('mvvm')).to.be.true;
+      expect(templateRequiresDecorators('mvp')).to.be.true;
+      expect(templateRequiresDecorators('tsx')).to.be.false;
+      expect(templateRequiresDecorators('jsx')).to.be.false;
+      expect(templateRequiresDecorators('js')).to.be.false;
     });
 
   });