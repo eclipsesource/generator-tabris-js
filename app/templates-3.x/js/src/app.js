d27f9c94a8ab0fa2fca2e72faf2bc66f8a46be5d
Tim Buschtoens <tbuschto@eclipsesource.com>
2021-09-28 15:15:59 +0200

    Update vanilla JavaScript template to use factory API
    Change-Id: Id749e798a355e16e8dc597bb977de33e6de52f67

--- app/templates-3.x/js/src/app.js
+++ app/templates-3.x/js/src/app.js
@@ -1,13 +1,13 @@
 const {Button, TextView, contentView} = require('tabris');
 
-new Button({
-  centerX: true, top: 100,
-  text: 'Show message'
-}).onSelect(() => {
-  textView.text = 'Tabris.js rocks!';
-}).appendTo(contentView);
-
-const textView = new TextView({
-  centerX: true, top: 'prev() 50',
-  font: '24px'
-}).appendTo(contentView);
+contentView.append(
+  Button({
+    centerX: true, top: 100,
+    text: 'Show message',
+    onSelect: () => $(TextView).only().text = 'Tabris.js rocks!'
+  }),
+  TextView({
+    centerX: true, top: 'prev() 50',
+    font: '24px'
+  })
+);
