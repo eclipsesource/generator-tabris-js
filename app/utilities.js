1ecc3fad962b443b0999145b39f8e4b5172adff8
Tim Buschtoens <tbuschto@eclipsesource.com>
2023-06-30 14:28:13 +0200

    Fix crash when git email is not configured
    "user.github.username()" can throw a synchroneous error, which was not
    caught.

--- app/utilities.js
+++ app/utilities.js
@@ -13,14 +13,19 @@ module.exports = {
 const VALID_APP_ID_REGEX = /^([a-z0-9_]+\.)+[a-z][a-z0-9_]+$/i;
 
 function toAppId(user, appname) {
-  return user.github.username()
-    .catch(() => user.git.email() ? user.git.email().split('@')[0] : null)
-    .then(username => username || 'org.example')
-    .then(domain => domain + '.' + appname.replace(/\s+/g, '').replace(/-+/g, '').toLowerCase())
-    .catch(error => {
-      console.log(error);
-      return '';
-    });
+  const appPostfix = appname.replace(/\s+/g, '').replace(/-+/g, '').toLowerCase();
+  try {
+    return user.github.username()
+      .catch(() => user.git.email() ? user.git.email().split('@')[0] : null)
+      .then(username => username || 'org.example')
+      .then(domain => domain + '.' + appPostfix)
+      .catch(error => {
+        console.log(error);
+        return '';
+      });
+  } catch (ex) {
+    return Promise.resolve('org.example.' + appPostfix);
+  }
 }
 
 function toName(string) {
